0d6728e6a3e11f5f943ff58601da133a20b87182
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-10-10 21:25:55 +0800

    Hari ke 2
    Hari ini saya membuat program menampilkan nama lengkap dan nama panggilan.

---
+++ DayTwo.java
@@ -0,0 +1,11 @@
+package Days;
+
+public class DayTwo {
+    public static void main(String[] args) {
+        String fullName = "Sintia";
+        String nickname = "Tia";
+        
+        System.out.println("Nama lengkap    : "+fullName);
+        System.out.println("Nama panggilan  : "+nickname);
+    }
+}
