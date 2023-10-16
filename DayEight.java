0a4acd6a415369b84f3d077fbb096c09c6bcff24
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-10-16 22:46:31 +0800

    Perulangan
    menampilkan angka genap

---
+++ DayEight.java
@@ -0,0 +1,15 @@
+package Days;
+
+public class DayEight {
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("Bilangan genap: ");
+        for (int i = 1; i <= a; i++) {
+            if (i % 2 == 0){
+                System.out.println(i);
+            
+            }
+        }
+    }
+}
