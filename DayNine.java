1b984ce602c476539544c260c32763a29b731de3
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-10-17 20:00:14 +0800

    Pengurutan data secara ascending
    hari ini saya membuat program tentang pengurutan elemen-elemen didalama array secara ascending

---
+++ DayNine.java
@@ -0,0 +1,16 @@
+package Days;
+
+import java.util.Arrays;
+
+public class DayNine {
+
+    public static void main(String[] args) {
+        int[]data = {23,17,4,29,26,93,176,9,26,18,64,19,2};
+        Arrays.sort(data);
+        System.out.println(Arrays.toString(data));
+        
+    
+    }
+}
+
+
