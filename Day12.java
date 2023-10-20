a43757b6d7943bdc92fb2a20054f14c2b42ef2ed
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-10-20 21:55:27 +0800

    Perulangan
    output dari program ini adalah menampilkan angka dari yang terbesar ke yang terkecil.

---
+++ Day12.java
@@ -0,0 +1,10 @@
+package Days;
+
+public class Day12 {
+    public static void main(String[] args) {
+        System.out.println("Mencetak sebuah nilai dari yang terbesar ke yang terkecil: ");
+        for (int i = 20; i >=1; i--) {
+            System.out.println(i);
+        }
+    }
+}
