48ea8957f3d538f3f0f63ee519d713c632d08cb4
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-10-21 23:22:42 +0800

    Perulangan for
    menampilkan bilangan ganjil

---
+++ Day13.java
@@ -0,0 +1,10 @@
+package Days;
+
+public class Day13 {
+    public static void main(String[] args) {
+        System.out.println("Mencetak bilangan ganjil: ");
+        for (int i = 1; i <=9; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
