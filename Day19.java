22318793a11d93d94fded22287ea440d641d7ba0
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-10-27 20:21:00 +0800

    perulangan for
    output dari program ini adalah mencetak kelipatan 3 dari 30 angka hingga 0

---
+++ Day19.java
@@ -0,0 +1,11 @@
+package Days;
+
+public class Day19 {
+    public static void main(String[] args) {
+        System.out.println("Menampilkan kelipatan 3: ");
+        for (int i = 30; i >=0; i -= 3) {
+            System.out.println(i);
+        }
+    }
+    
+}
