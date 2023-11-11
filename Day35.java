b5979853a38203fa98e2eaaafeaad8224461d438
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-11-12 06:38:42 +0800

    boolean
    program ini sangat sederhana yang akan mencetak beberapa pertanyaan tentang belajar Java dan akan menampilkann output yang telah ditentukan.

---
+++ Day35.java
@@ -0,0 +1,11 @@
+package Days;
+
+public class Day35 {
+    public static void main(String[] args) {
+        boolean javaTesti = true;
+        boolean testiJava = false;
+        
+        System.out.println("Apakah belajar java menyenangkan? " + javaTesti);
+        System.out.println("Apakah belajar java melelahkan? " + testiJava);
+    }
+}
