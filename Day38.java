8cb9dad684487bcd86b437e4179ed27a626c00d5
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-11-15 07:12:42 +0800

    tipe data byte
    program ini menggunakan tipe data byte untuk menjalankan sebuah operasi aritmatika sederhana.

---
+++ Day38.java
@@ -0,0 +1,22 @@
+package Days;
+
+
+
+public class Day38 {
+    public static void main(String[] args) {
+        byte angkaPertama = 10;
+        byte angkaKedua = 7;
+        
+        byte penjumlahan =  (byte) (angkaPertama + angkaKedua);
+        System.out.println("Penjumlahan: " + penjumlahan);
+        
+        byte pengurangan = (byte) (angkaPertama - angkaKedua);
+        System.out.println("Pengurangan: " + pengurangan);
+        
+        byte perkalian = (byte) (angkaPertama * angkaKedua);
+        System.out.println("Perkalian: " + perkalian);
+        
+        byte pembagian = (byte) (angkaPertama / angkaKedua);
+        System.out.println("Pembagian: " + pembagian);
+    }
+}
