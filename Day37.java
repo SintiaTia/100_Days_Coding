f767f4634ec898718b4566af06db778fcb3bf9a6
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-11-14 21:21:41 +0800

    tipe data byte
    program ini untuk membantu pengguna mengetahui nilai maksimum dan nilai minimum dari tipe data byte.

---
+++ Day37.java
@@ -0,0 +1,11 @@
+package Days;
+
+public class Day37 {
+    public static void main(String[] args) {
+         byte minValue = Byte.MIN_VALUE;
+         byte maxValue = Byte.MAX_VALUE;
+         
+         System.out.println("Nilai minimum dari tipe data byte: "+minValue);
+         System.out.println("Nilai maksimum dari tipe data byte: "+maxValue);
+    }
+}
