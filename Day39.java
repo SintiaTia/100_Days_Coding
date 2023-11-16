716a52bc3e85e7fc7406f0251e54a82064330712
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-11-16 21:15:55 +0800

    Array dengan tipe data integer
    program ini  membuat sebuah array 1 dimensi dimana panjang dari array tersebut adalah 5, kemudian mencetak hasil dari array tersebut.

---
+++ Day39.java
@@ -0,0 +1,18 @@
+package Days;
+
+
+public class Day39 {
+    public static void main(String[] args) {
+        int [] angka = new int[5];
+        angka[0] = 10;
+        angka[1] = 20;
+        angka[2] = 30;
+        angka[3] = 40;
+        angka[4] = 50;
+        System.out.println("Isi dari array: ");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Elemen array ke- " + i + ": " + angka[i]);
+        }
+    }
+    
+}
