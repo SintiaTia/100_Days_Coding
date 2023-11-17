eb857dd1c84855fffedff170f0520e6f916fbebe
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-11-17 23:28:49 +0800

    for
    menampilkan angka genap antara 3 sampai 100.

---
+++ Day40.java
@@ -0,0 +1,12 @@
+package Days;
+
+public class Day40 {
+    public static void main(String[] args) {
+        for (int s = 3; s <= 100; s++) {
+            if(s % 2 ==0){
+                System.out.println(s);
+            }
+        }
+    }
+    
+}
