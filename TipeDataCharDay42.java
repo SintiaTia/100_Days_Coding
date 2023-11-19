cfbbc3a7886d2d36230eb91925166ac79094d8ea
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-11-19 23:13:26 +0800

    Day42
    program ini akan menampilkan karakter-karakter dalam rentang tertentu (misalnya, dari 'S' sampai 'Z').

---
+++ TipeDataCharDay42.java
@@ -0,0 +1,13 @@
+package Days;
+
+public class TipeDataCharDay42 {
+    public static void main(String[] args) {
+        char startKarakter = 'S';
+        char endKarakter = 'Z';
+        
+        for (char i = startKarakter; i <= endKarakter; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println(" ");
+    }
+}
