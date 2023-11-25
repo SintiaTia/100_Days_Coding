fb2db0aef6352f276fccbaa9ad6d93062c0aa0b4
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-11-25 18:38:18 +0800

    Day48
    program ini akan membantu pengguna untuk menghitung atau mengetahui jumlah populasi jiwa yang ada di bumi ini dengan menggunakan tipe data long.

---
+++ MenghitungPopulasiDuniaDay48.java
@@ -0,0 +1,8 @@
+package Days;
+
+public class MenghitungPopulasiDuniaDay48 {
+    public static void main(String[] args) {
+        long populasiManusiaDiDunia = 8005176000L;
+        System.out.println("Perkiraan populasi Manusia yang ada didunia adalah : " + populasiManusiaDiDunia + " jiwa" );
+    }
+}
