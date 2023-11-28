d3975d0465204a91f71c0c2e591edd37df00d435
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-11-28 16:21:41 +0800

    Day51
    Program ini untuk menampilkan elemen-elemen yang ada didalam Array 1 dimensi.

---
+++ MenampilkanElemenArrayDay51.java
@@ -0,0 +1,14 @@
+package Days;
+
+public class MenampilkanElemenArrayDay51 {
+    public static void main(String[] args) {
+         //Program Menampilkan Elemen Array
+         int sintia [] = {9,7,2,4,8,3};
+         
+         System.out.print("Elemen-elemen dalam array: ");
+         for (int i = 0; i < sintia.length; i++) {
+             System.out.print(sintia[i] + ",");
+        }
+         System.out.println("");
+    }
+}
