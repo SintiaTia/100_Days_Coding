700baf621bb3b6fd5c8cae8670a2028bafb771cd
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-12-18 20:18:55 +0800

    Day71
    Program ini akan meminta pengguna untuk memasukkan panjang dan lebar persegi panjang, dan kemudian menghitung dan menampilkan luasnya.

---
+++ LuasPersegiPanjangDay71.java
@@ -0,0 +1,17 @@
+package Days;
+
+import java.util.Scanner;
+
+public class LuasPersegiPanjangDay71 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan panjang persegi panjang: ");
+        double panjang = scanner.nextDouble();
+        
+        System.out.print("Masukkan lebar persegi panjang: ");
+        double lebar = scanner.nextDouble();
+        
+        double luas = panjang*lebar;
+        System.out.println("Luas dari persegi panjang tersebut adalah: " + luas);
+    }
+}
