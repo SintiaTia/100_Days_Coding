edbac72a64cba23c07fa8c6436f87466ffcf3617
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-12-21 15:57:27 +0800

    Add files via upload
    Program yang melakukan operasi aritmatika penjumlahan dua angka dan kemudian mencetak hasilnya ke layar.

---
+++ aritmatikaDay74.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Days;
+
+/**
+ *
+ * @author acer
+ */
+public class aritmatikaDay74 {
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 3;
+        int hasil = angka1 + angka2;
+        System.out.println("Hasil: " + hasil);
+    }
+}
+
+
