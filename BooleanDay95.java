1a1bd91882cb366ce5ab4eb49762b683e8a80482
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2024-01-11 20:25:43 +0800

    Day95
    Program diatas untuk menentukan apakah nilai pada 2 variabel pada program bernilai  true atau false, dengan menggunakan tipe data boolean.

---
+++ BooleanDay95.java
@@ -0,0 +1,16 @@
+package Days;
+
+public class BooleanDay95 {
+    public static void main(String[] args) {
+        int angka = 7;
+        int bilangan = 3;
+        
+        boolean I = (angka == bilangan);
+        boolean L = (angka > bilangan);
+        boolean Y = (angka < bilangan);
+        
+        System.out.println("apakah angka sama dengan bilangan? " + I);
+        System.out.println("apakah angka lebih besar dari bilangan? " + L);
+        System.out.println("apakah angka lebih kecil dari bilangan? " + Y);
+    }
+}
