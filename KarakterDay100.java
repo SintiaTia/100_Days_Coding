b18453b3da8dd91b5b87fa5fed012524dcd72107
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2024-01-16 22:13:27 +0800

    Day100
    Program ini akan mencetak karakter dari 'A' hingga 'Z' ke layar dengan menggunakan perulangan for.

---
+++ KarakterDay100.java
@@ -0,0 +1,12 @@
+package Days;
+
+public class KarakterDay100 {
+    public static void main(String[] args) {
+        karakter();
+    }
+    public static void karakter(){
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.println(i);
+        }
+    }
+}
